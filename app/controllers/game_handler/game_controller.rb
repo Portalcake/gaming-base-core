19059ac324adbd60ededb7910a7e34cb07f18932
Noxx <webmaster@gaming-base.net>
2012-12-29 17:10:11 +0100

    Hopefilly fixed controller-issues between games

---
+++ app/controllers/game_handler/game_controller.rb
@@ -0,0 +1,30 @@
+module GameHandler
+  class GameController < ApplicationController
+    before_filter :load_game
+
+    def load_game
+      return unless game_id
+      unless @game = ::Game.find_by_token(game_id)
+        respond_to do |format|
+          format.html {
+            redirect_to root_url,
+              :alert => 'Game not supported.',
+              :status => :not_found
+          }
+          format.json { head :not_found }
+        end
+        return false
+      end
+      unless @game.published?
+        render 'share/game_unpublished', :layout => 'application'
+      end
+      if @game.maintenance?
+        render "share/game_maintenance", :layout => 'application'
+      end
+    end
+
+    def game_id
+      nil #need to be overridden
+    end
+  end
+end
