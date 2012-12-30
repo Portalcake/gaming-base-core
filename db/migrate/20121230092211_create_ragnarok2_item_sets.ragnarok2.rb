caaf18c60722df94974eb5b832187f3c35426ac9
Noxx <webmaster@gaming-base.net>
2012-12-30 11:52:46 +0100

    Added Game:Ro2 migrations for Set-Items

---
+++ db/migrate/20121230092211_create_ragnarok2_item_sets.ragnarok2.rb
@@ -0,0 +1,13 @@
+# This migration comes from ragnarok2 (originally 20121230091722)
+class CreateRagnarok2ItemSets < ActiveRecord::Migration
+  def change
+    create_table :ragnarok2_item_sets do |t|
+
+      t.integer :set_id, :default=>0, :null=>false
+      t.integer :string_id, :limit=>8, :default=>0, :null=>false
+      t.timestamps
+    end
+
+    add_index :ragnarok2_item_sets, :set_id, :unique => true
+  end
+end
---
+++ db/migrate/20121230101057_create_ragnarok2_translations_item_sets.ragnarok2.rb
@@ -0,0 +1,12 @@
+# This migration comes from ragnarok2 (originally 20121230100728)
+class CreateRagnarok2TranslationsItemSets < ActiveRecord::Migration
+  def change
+    create_table :ragnarok2_translations_item_sets do |t|
+      t.integer :item_set_id, :default => 0, :null => false, :limit => 8
+      t.string :translation
+
+      t.timestamps
+    end
+    add_index :ragnarok2_translations_item_sets, :item_set_id, :unique => true, :name => :sid
+  end
+end
