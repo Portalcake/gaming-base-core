10da6defeb345cb29821fcf28dd7385a7f05d281
Noxx <webmaster@gaming-base.net>
2013-01-17 21:07:14 +0100

    Added missing files of last commit

---
+++ db/migrate/20130117185644_create_ragnarok2_translations_kharas.ragnarok2.rb
@@ -0,0 +1,15 @@
+# This migration comes from ragnarok2 (originally 20130117185345)
+class CreateRagnarok2TranslationsKharas < ActiveRecord::Migration
+  def change
+    create_table :ragnarok2_translations_kharas do |t|
+
+      t.integer :quest_id, :default=>0, :null=>false
+      t.string :title
+      t.string :summary
+
+      t.timestamps
+    end
+
+    add_index :ragnarok2_translations_kharas, :quest_id, :unique=>true
+  end
+end
