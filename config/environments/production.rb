93c3253e4b3151715e568129b928fa9ceed2118f
Noxx <webmaster@gaming-base.net>
2013-01-25 12:06:58 +0100

    Changed log-level forproduction to :warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Gamingbase::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
